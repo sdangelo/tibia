1102d9f186ed26ab320e5d7af9968de3535e83c5
Stefano D'Angelo <stefano.dangelo@orastron.com>
2024-01-20 07:06:21 +0100

    https in web demo

--- templates/web-demo/demo.mk
+++ templates/web-demo/demo.mk
@@ -1,4 +1,9 @@
-ALL += build/index.html
+ALL += build/index.html build/cert.pem build/key.pem
 
 build/index.html: src/index.html | build
 	cp $^ $@
+
+build/key.pem: build/cert.pem
+
+build/cert.pem: | build
+	yes "" | openssl req -x509 -newkey rsa:2048 -keyout build/key.pem -out build/cert.pem -days 365 -nodes 2>/dev/null
