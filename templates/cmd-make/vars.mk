f1281168713a4db68a00d022a797c06f9b04d126
Stefano D'Angelo <stefano.dangelo@orastron.com>
2024-02-01 06:16:50 +0100

    added options to tibia and made cmd-make flexible

--- templates/cmd-make/vars.mk
+++ templates/cmd-make/vars.mk
@@ -1,5 +1,8 @@
 BUNDLE_NAME := {{=it.product.bundleName}}
 CFLAGS_EXTRA := {{=it.make && it.make.cflags ? it.make.cflags : ""}} {{=it.cmd_make.cflags ? it.cmd_make.cflags : ""}}
 LDFLAGS_EXTRA := {{=it.make && it.make.ldflags ? it.make.ldflags : ""}} {{=it.cmd_make.ldflags ? it.cmd_make.ldflags : ""}}
+MAIN_DIR := {{=it.cmd_make && it.cmd_make.mainDir ? it.cmd_make.mainDir : (it.make && it.make.mainDir ? it.make.mainDir : "")}}
+DATA_DIR := {{=it.cmd_make && it.cmd_make.dataDir ? it.cmd_make.dataDir : (it.make && it.make.dataDir ? it.make.dataDir : "")}}
+PLUGIN_DIR := {{=it.cmd_make && it.cmd_make.pluginDir ? it.cmd_make.pluginDir : (it.make && it.make.pluginDir ? it.make.pluginDir : "")}}
 TINYWAV_DIR := {{=it.cmd_make.tinywavDir}}
 MIDI_PARSER_DIR := {{=it.cmd_make.midiParserDir}}
