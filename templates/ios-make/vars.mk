b624b120880d7eef751473c2ec7f50629170fc78
Stefano D'Angelo <s@dangelo.audio>
2024-02-22 12:31:35 +0100

    copyright notices

--- templates/ios-make/vars.mk
+++ templates/ios-make/vars.mk
@@ -1,3 +1,23 @@
+#
+# Tibia
+#
+# Copyright (C) 2024 Orastron Srl unipersonale
+#
+# Tibia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# Tibia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tibia.  If not, see <http://www.gnu.org/licenses/>.
+#
+# File author: Stefano D'Angelo
+#
+
 BUNDLE_NAME := {{=it.product.bundleName}}
 
 C_SRCS_EXTRA := {{=it.make && it.make.cSrcs ? it.make.cSrcs : ""}} {{=it.ios_make && it.ios_make.cSrcs ? it.ios_make.cSrcs : ""}}
